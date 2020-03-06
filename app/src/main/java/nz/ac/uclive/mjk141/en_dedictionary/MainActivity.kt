2cb28acf7b68acb3a540f97163448d1341abbd67
Matthew Kenny <mjk141@uclive.ac.nz>
2020-03-07 12:21:11 +1300

    Changed project structure. Included an empty activity.

---
+++ app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/MainActivity.kt
@@ -0,0 +1,12 @@
+package nz.ac.uclive.mjk141.en_dedictionary
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
