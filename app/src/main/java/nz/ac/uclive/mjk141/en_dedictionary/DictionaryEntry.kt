e192bf119172dcf07f323f08f82ca0cbb4cff900
Matthew Kenny <mjk141@uclive.ac.nz>
2020-03-13 19:08:08 +1300

    Created DictionaryEntry class and a file of entries.

---
+++ app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/DictionaryEntry.kt
@@ -0,0 +1,4 @@
+package nz.ac.uclive.mjk141.en_dedictionary
+
+data class DictionaryEntry(val term: String, val translation: String, val genderArticle: String) {
+}
---
+++ app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/entires.kt
@@ -0,0 +1,52 @@
+package nz.ac.uclive.mjk141.en_dedictionary
+
+object dictionaryEntries {
+    private const val MASCULINE = "der"
+    private const val FEMININE = "die"
+    private const val NEUTRAL = "das"
+
+    val entries = arrayOf(
+        DictionaryEntry("Alligator", "Alligator", MASCULINE),
+        DictionaryEntry("Ant", "Ameise", FEMININE),
+        DictionaryEntry("Bear", "Bär", MASCULINE),
+        DictionaryEntry("Bird", "Vogel", MASCULINE),
+        DictionaryEntry("Cat", "Katze", FEMININE),
+        DictionaryEntry("Cow", "Kuh", FEMININE),
+        DictionaryEntry("Deer", "Reh", NEUTRAL),
+        DictionaryEntry("Dog", "Hund", MASCULINE),
+        DictionaryEntry("Donkey", "Esel", NEUTRAL),
+        DictionaryEntry("Eagle", "Adler", MASCULINE),
+        DictionaryEntry("Elephant", "Elefant", MASCULINE),
+        DictionaryEntry("Fish", "Fisch", MASCULINE),
+        DictionaryEntry("Fox", "Fuchs", MASCULINE),
+        DictionaryEntry("Giraffe", "Giraffe", FEMININE),
+        DictionaryEntry("Goat", "Ziege", FEMININE),
+        DictionaryEntry("Hare", "Hase", MASCULINE),
+        DictionaryEntry("Horse", "Pferd", NEUTRAL),
+        DictionaryEntry("Jaguar", "Jaguar", MASCULINE),
+        DictionaryEntry("Kangaroo", "Känguru", NEUTRAL),
+        DictionaryEntry("Kiwi", "Kiwi", FEMININE),
+        DictionaryEntry("Leopard", "Leopard", MASCULINE),
+        DictionaryEntry("Lion", "Löwe", MASCULINE),
+        DictionaryEntry("Lynx", "Luchs", MASCULINE),
+        DictionaryEntry("Manta Ray", "Mantarochen", MASCULINE),
+        DictionaryEntry("Mole", "Maulwurf", MASCULINE),
+        DictionaryEntry("Newt", "Molch", MASCULINE),
+        DictionaryEntry("Octopus", "Oktopus", MASCULINE),
+        DictionaryEntry("Ostrich", "Strauß", MASCULINE),
+        DictionaryEntry("Parrot", "Papagei", MASCULINE),
+        DictionaryEntry("Pig", "Schwein", NEUTRAL),
+        DictionaryEntry("Quail", "Wachtel", FEMININE),
+        DictionaryEntry("Rabbit", "Kaninchen", NEUTRAL),
+        DictionaryEntry("Rhinoceros", "Nashorn", NEUTRAL),
+        DictionaryEntry("Seal", "Siegel", NEUTRAL),
+        DictionaryEntry("Snake", "Schlange", FEMININE),
+        DictionaryEntry("Tiger", "Tiger", MASCULINE),
+        DictionaryEntry("Tortoise", "Schildkröte", FEMININE),
+        DictionaryEntry("Vulture", "Geier", MASCULINE),
+        DictionaryEntry("Weasel", "Wiesel", MASCULINE),
+        DictionaryEntry("Wolf", "Wolf", MASCULINE),
+        DictionaryEntry("Yak", "Yak", MASCULINE),
+        DictionaryEntry("Zebra", "Zebra", NEUTRAL)
+    )
+}
