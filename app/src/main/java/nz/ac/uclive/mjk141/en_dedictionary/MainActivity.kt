c2e7d6c955410104d4d4cde21bdbb6705b60f095
Matthew Kenny <mjk141@uclive.ac.nz>
2020-03-13 19:08:36 +1300

    Worked towards RecyclerView

--- app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/MainActivity.kt
@@ -4,16 +4,24 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : Activity() {
+    private lateinit var dictionaryRecycler: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private val startingWords =
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewManager = LinearLayoutManager(this)
+        //viewAdapter = MyA
         val searchButton: Button = findViewById(R.id.searchButton)
         searchButton.setOnClickListener {
-            print("THIS IS A TEST===================")
             onSearchRequested()
         }
 
