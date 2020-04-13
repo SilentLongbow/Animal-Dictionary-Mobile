50f3ff03c2fb37fa018b0289391d256d4f7ccfbb
Matthew Kenny <mjk141@uclive.ac.nz>
2020-04-14 02:04:03 +1200

    Added boilerplate for navigation.

--- app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/MainActivity.kt
@@ -1,28 +1,21 @@
 package nz.ac.uclive.mjk141.en_dedictionary
 
-import android.os.Bundle
-import android.view.Menu
-import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
-import nz.ac.uclive.mjk141.en_dedictionary.main_page_recycler.DictionaryAdapter
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        val dictionaryArray = Translations.entries
-        dictionaryView.adapter = DictionaryAdapter(dictionaryArray)
-        dictionaryView.layoutManager = LinearLayoutManager(this)
-        dictionaryView.setHasFixedSize(true)
-
-    }
-
-    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.options_menu, menu)
-        return true
+        navController = this.findNavController(R.id.navigationFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
