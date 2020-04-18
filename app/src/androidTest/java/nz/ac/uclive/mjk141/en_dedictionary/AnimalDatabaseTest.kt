3276fe2f747d231d59839b0ac2f66f852f576855
Matthew Kenny <mjk141@uclive.ac.nz>
2020-04-18 21:24:53 +1200

    Removed gender-based tests after design change.

--- app/src/androidTest/java/nz/ac/uclive/mjk141/en_dedictionary/AnimalDatabaseTest.kt
+++ app/src/androidTest/java/nz/ac/uclive/mjk141/en_dedictionary/AnimalDatabaseTest.kt
@@ -4,21 +4,17 @@ import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import nz.ac.uclive.mjk141.en_dedictionary.animal_database.AnimalDatabase
-import nz.ac.uclive.mjk141.en_dedictionary.animal_database.AnimalDatabaseDao
-import nz.ac.uclive.mjk141.en_dedictionary.animal_database.GrammaticalGender
-import nz.ac.uclive.mjk141.en_dedictionary.utils.Gender
+import nz.ac.uclive.mjk141.en_dedictionary.animal_database.dao.AnimalDao
 import org.junit.After
-import org.junit.Assert
 import org.junit.Before
-import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
-import java.lang.Exception
+
 
 @RunWith(AndroidJUnit4::class)
 class AnimalDatabaseTest {
 
-    private lateinit var animalDao: AnimalDatabaseDao
+    private lateinit var animalDao: AnimalDao
     private lateinit var db: AnimalDatabase
 
     @Before
@@ -28,21 +24,12 @@ class AnimalDatabaseTest {
         db = Room.inMemoryDatabaseBuilder(context, AnimalDatabase::class.java)
             .allowMainThreadQueries()
             .build()
-        animalDao = db.animalDatabaseDao
+        animalDao = db.animalDao
     }
 
     @After
     @Throws(IOException::class)
     fun closeDb() {
         db.close()
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun insertAndGetGender() {
-        val gender = GrammaticalGender(Gender.FEMININE.name, Gender.FEMININE.str)
-        animalDao.insert(gender)
-        val newGender = animalDao.getGender(Gender.FEMININE.name)
-        Assert.assertEquals(gender, newGender)
     }
 }
