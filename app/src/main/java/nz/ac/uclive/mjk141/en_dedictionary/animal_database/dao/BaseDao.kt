0063b7262d03f1ae0d0aa4c6de1cd84ffb2e4db8
Matthew Kenny <mjk141@uclive.ac.nz>
2020-04-18 21:30:13 +1200

    Created a base dao and updated created a specific animal dao. Both are in a DAO specific folder and removed others.

---
+++ app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/animal_database/dao/BaseDao.kt
@@ -0,0 +1,42 @@
+package nz.ac.uclive.mjk141.en_dedictionary.animal_database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+
+@Dao
+interface BaseDao<T> {
+
+    /**
+     * Insert an object in the database.
+     *
+     * @param obj the object to be inserted.
+     */
+    @Insert
+    fun insert(obj: T)
+
+    /**
+     * Insert an array of objects in the database.
+     *
+     * @param obj the objects to be inserted.
+     */
+    @Insert
+    fun insert(vararg obj: T)
+
+    /**
+     * Update an object from the database.
+     *
+     * @param obj the object to be updated
+     */
+    @Update
+    fun update(obj: T)
+
+    /**
+     * Delete an object from the database
+     *
+     * @param obj the object to be deleted
+     */
+    @Delete
+    fun delete(obj: T)
+}
