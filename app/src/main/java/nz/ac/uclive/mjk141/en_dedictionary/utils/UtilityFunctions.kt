8ef296638a985d74e2ac221df6f6df05f70cccbe
Matthew Kenny <mjk141@uclive.ac.nz>
2020-04-20 13:55:37 +1200

    Created a utility function file for hiding soft keyboard.

---
+++ app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/utils/UtilityFunctions.kt
@@ -0,0 +1,10 @@
+package nz.ac.uclive.mjk141.en_dedictionary.utils
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun hideSoftKeyboard(view: View, context: Context) {
+    val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
