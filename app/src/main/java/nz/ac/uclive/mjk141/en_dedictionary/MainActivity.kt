467c010dde2ca0fb7a2369e6dc5684c2f727f6fd
Matthew Kenny <mjk141@uclive.ac.nz>
2020-04-23 14:18:51 +1200

    Cleaned up Main activity

--- app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/mjk141/en_dedictionary/MainActivity.kt
@@ -3,27 +3,17 @@ package nz.ac.uclive.mjk141.en_dedictionary
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.toolbar.*
-import kotlinx.coroutines.*
-import nz.ac.uclive.mjk141.en_dedictionary.animal_database.AnimalDatabase
-import nz.ac.uclive.mjk141.en_dedictionary.animal_database.dao.AnimalDao
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var prefs: SharedPreferences
-
-    private lateinit var animalDatabase: AnimalDatabase
-    private lateinit var animalDao: AnimalDao
-    private val mainJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + mainJob)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +21,11 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(custom_toolbar)
 
         navController = this.findNavController(R.id.navigationFragment)
+
         appBarConfiguration = AppBarConfiguration(navController.graph)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
 
-        animalDatabase = AnimalDatabase.getInstance(applicationContext)
-        animalDao = animalDatabase.animalDao
-
         prefs = getSharedPreferences("nz.ac.uclive.mjk141.en_dedictonary", Context.MODE_PRIVATE)
-
-        //val assetList = assets.list("animal_images/")
 
     }
 
@@ -58,9 +44,5 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun populateDatabase() {
-
-        uiScope.launch {
-
-        }
     }
 }
